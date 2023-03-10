0b7e76f1e49fe77aab2ea8c7cc33555178a754df
Victor Herrera <victor.herrera@ucuenca.edu.ec>
2023-03-10 11:59:34 -0500

    Cambiado nombre de ruta users a usuarios. Cambiado mensaje de inicial de ruta index.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'API Sistema Nutrición Neo4j' });
 });
 
 module.exports = router;
